a9c3a7e35a24e5185301f4d4adeab0042bad916c
tallis-bowers <tallis.bowers.7@gmail.com>
2019-02-07 13:52:01 -0500

    updates to Hobby page
    added image and text

--- script1.js
+++ script1.js
@@ -2,6 +2,12 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.display = "none";
+  
+  var hdivs = document.getElementsByClassName("hobby_div");
+        for(var i = 0; i < hdivs.length; i++) {
+            var hdiv = hdivs[i];
+            hdiv.style.marginLeft = "200px";
+        }
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -9,4 +15,10 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.display = "block";
   document.getElementById("main").style.marginLeft = "0";
+  
+  var hdivs = document.getElementsByClassName("hobby_div");
+        for(var i = 0; i < hdivs.length; i++) {
+            var hdiv = hdivs[i];
+            hdiv.style.marginLeft = "0px";
+        }
 }
